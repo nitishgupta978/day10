627ad25a13792e2ac4c9c977090b6123d2c891d2
nitishgupta978 <nitishgupta9785@gmail.com>
2022-02-15 22:47:05 +0530

    Added for Employe wage Salary

--- 451batch/att.sh
+++ 451batch/att.sh
@@ -1,8 +1,15 @@
-ispersent=1
+ratePrHr=20
+present=1
 check=$((RANDOM%2))
-if [$check -eq $ispresent ]
+if [ $check -eq $present ]
 then
 	echo "Employe is present"
+
+	WrPrHr=8
 else
 	echo "Employe is absent"
+
+	WrPrHr=0
 fi
+Salary=$(($ratePrHr*WrPrHr))
+echo "Daily Salry is $Salary"
